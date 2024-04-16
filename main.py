0f46d2a700d31e3059b2d6889c08675acdb62c9b
Kanupriya68 <73285758+Kanupriya68@users.noreply.github.com>
2024-04-16 12:43:23 +0100

    Add main.py
    Contains base Python imports and a function to run predict scan tool

---
+++ main.py
@@ -0,0 +1,26 @@
+import subprocess
+import csv
+import re
+
+rows = []
+def mhcflurry(protein:str,allele:str) -> None:
+    if not re.match("^HLA-[A-Z]\*\d{2}:\d{2,3}$", allele):
+        raise ValueError(
+            "Allele name has to have the form of XXX-X*00:00")
+    elif not re.match("^[ACDEFGHIKLMNPQRSTVWY]+$", protein):
+        raise ValueError(
+            "Incorrect protein sequence/Not a protein sequence")
+    else:
+        subprocess.check_output(["mhcflurry-predict-scan", "--sequences", protein
+                                    , "--alleles", allele, "--out", "output.csv"])
+        with open("output.csv", 'r') as file:
+            csvreader = csv.reader(file)
+            header = next(csvreader)
+            for row in csvreader:
+                rows.append(row)
+        return [header,rows]
+
+
+# result = mhcflurry('MFVFLVLLPLVSSQCVNLTTRTQLPPAYTNSFTRGVYYPDKVFRSSV','HLA-A*02:01')
+# print(result)
+
