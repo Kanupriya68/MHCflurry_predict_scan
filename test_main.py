d88ed3928478b9ca6324b510119fa571c600e7ef
Kanupriya68 <73285758+Kanupriya68@users.noreply.github.com>
2024-04-18 15:00:51 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,16 +4,16 @@ import unittest
 
 class mhcflurry_Test_Class(unittest.TestCase):
    def test_header(self):
-       #Checks whether data is populated or not
+       #Checks whether header columns are populated or not
        return_value = main.mhcflurry("MFVFLVLLPLVSSQCVNLTTRTQLPPAYTNSFTRGVYYPDKVFRSSVL", "HLA-A*02:01")
        self.assertTrue(return_value[0])
    def test_csv_rows(self):
-       #Checks whether data is populated or not
+       #Checks whether rows are populated or not
        return_value = main.mhcflurry("MFVFLVLLPLVSSQCVNLTTRTQLPPAYTNSFTRGVYYPDKVFRSSVL", "HLA-A*02:012")
        self.assertTrue(return_value[1:])
 
 
-# i will use code below to run the unit test
+# Run test
 t = mhcflurry_Test_Class()
 suite = unittest.TestLoader().loadTestsFromModule(t)
 unittest.TextTestRunner().run(suite)
